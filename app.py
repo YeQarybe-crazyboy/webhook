32764af627f428b7aa3ea9d17b1a3013b9d81e7c
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2025-07-04 14:45:11 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Body
+from requests import post
+
+app = FastAPI()
+
+@app.post('/')
+async def send(req: dict=Body(...,embed=False)):
+    post('http://46.8.69.112:8000/Telegram?key=DEFAULT', data=dict(req))
+    return {'ok': True}
